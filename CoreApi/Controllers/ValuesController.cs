96f27e1d9a629bb12f470a067bc1f76cd5117b43
rahmatafridi <khawry900@gmail.com>
2019-05-23 20:34:03 +0500

    Initial Commit

---
+++ CoreApi/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        // GET api/values
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values=_context.values.ToList();
+            return Ok(values);
+        }
+       
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var values=_context.values.FirstOrDefault(x=>x.Id==id);
+            return Ok(values);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CoreApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CoreApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApi.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) { }
+        public DbSet<Value> values { get; set; }
+    }
+}
